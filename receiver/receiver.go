f630e8baf42cfbace0770a11c368805af0a30e7c
aeneasr <aeneas.rekkas@serlo.org>
2015-03-23 16:18:14 +0100

    receiver: initial commit

---
+++ receiver/receiver.go
@@ -0,0 +1,45 @@
+package receiver
+
+import (
+	"code.google.com/p/go-uuid/uuid"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/ory-platform/google-json-style-response/responder"
+	"io"
+)
+
+type Receiver interface {
+	GetResponse(r io.Reader) (*Response, error)
+}
+
+type Response struct {
+	ApiVersion string          `json:"apiVersion"`
+	Id         uuid.UUID       `json:"id,omitempty"`
+	Data       interface{}     `json:"data,omitempty"`
+	Error      responder.Error `json:"error,omitempty"`
+}
+
+type receiver struct {
+	apiVersion string
+}
+
+func New(apiVersion string) Receiver {
+	return &receiver{
+		apiVersion: apiVersion,
+	}
+}
+
+func (r *receiver) GetResponse(reader io.Reader) (*Response, error) {
+	decoder := json.NewDecoder(reader)
+	response := new(Response)
+	err := decoder.Decode(response)
+	if err != nil {
+		return nil, err
+	}
+	if r.apiVersion != response.ApiVersion {
+		return nil, errors.New(fmt.Sprintf("API versions mismatch. Expected %s but got %s", r.apiVersion, response.ApiVersion))
+	}
+
+	return response, err
+}
---
+++ receiver/receiver_test.go
@@ -0,0 +1,51 @@
+package receiver
+
+import (
+	"bytes"
+	"github.com/ory-platform/google-json-style-response/responder"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type DataMock struct {
+	A string
+	B string
+}
+
+func TestNew(t *testing.T) {
+	r := New("1.0")
+	assert.NotNil(t, r)
+}
+
+func TestGetResponse(t *testing.T) {
+	rc := responder.New("1.0")
+	o := rc.Success(DataMock{
+		A: "a",
+		B: "b",
+	})
+	b, err := o.Marshal()
+	assert.Nil(t, err)
+
+	r := New("1.0")
+	reader := bytes.NewReader(b)
+	result, err := r.GetResponse(reader)
+	assert.Nil(t, err)
+
+	do := result.Data.(map[string]interface{})
+	assert.Equal(t, "a", do["A"])
+}
+
+func TestGetResponseFailsOnAPIVersionMismatch(t *testing.T) {
+    rc := responder.New("1.0")
+    o := rc.Success(DataMock{
+        A: "a",
+        B: "b",
+    })
+    b, err := o.Marshal()
+    assert.Nil(t, err)
+
+    r := New("1.1")
+    reader := bytes.NewReader(b)
+    _, err = r.GetResponse(reader)
+    assert.NotNil(t, err)
+}
